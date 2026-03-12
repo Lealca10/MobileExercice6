96196b12274d7d7444ea39123acb03cf0c828ba0
leandro.alcantara <leandro.alcantara14@gmail.com>
2026-03-12 19:39:00 -0300

    Alinhamento READ MORE

--- app/src/main/java/com/senac/exercicio6/MainActivity.kt
+++ app/src/main/java/com/senac/exercicio6/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -112,7 +113,9 @@ fun BookScreen() {
 
             Text(
                 text = "READ MORE",
-                color = Color.Blue
+                color = Color.Blue,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
             )
         }
     }
